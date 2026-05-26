3c8d7be79d7c40d1172200747f4cacc570ec2c88
Enes BUZLUK <139135923+cysctl@users.noreply.github.com>
2026-05-26 23:58:06 +0300

    feat: add base formatter class and text and latex formats

---
+++ boolbox/formatters/latex.py
@@ -0,0 +1,20 @@
+from boolbox.formatters.base import Formatter
+
+
+class LaTeXFormatter(Formatter):
+    """
+    LaTeX formatter for polynomials
+    Replaces 'XOR' with '\oplus'
+    """
+
+    def format(self, polys: dict[int, str]) -> str:
+        data = []
+
+        for idx, poly in polys.items():
+            # Replace 'XOR' with '\oplus'
+            #
+            new_poly_str = poly.replace(" XOR ", " \\oplus ")
+
+            data.append(f"x_{idx} -> S_{idx} = {new_poly_str}")
+
+        return "\n".join(data)
